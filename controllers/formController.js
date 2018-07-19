10f0fe427ab39cb22b112fdb81e04858da744d61
SL0TR <mohaimin94@gmail.com>
2018-07-19 15:01:26 +0600

    add form with ejs

---
+++ controllers/formController.js
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser');
+// const urlencodeParser = bodyParser.urlencoded({ extended: false});
+
+
+let data = [
+  {
+    name: 'Mohaimin Islam',
+    age: 24
+  }
+]
+
+module.exports = (app) => {
+  app.get('/form', (req, res) => {
+
+    res.render('form', {form: data});
+
+    // get data from mongodb and pass it to the view
+    // Todo.find({}, (err, data) => {
+    //   if(err) throw err;
+    //   res.render('todo', {todos: data});
+
+    // })
+  });
+
+  // app.post('/', urlencodeParser, (req, res) => {
+    
+  //   // get data from the view and add it to mongodb
+  //   let newTodo = Todo(req.body).save( (err, data) => {
+  //     if (err) throw err;
+  //     res.json(data);
+  //   })
+  // });
+
+  // app.delete('/:item', (req, res) => {
+
+  //   let deletedItem = req.params.item;
+  //   deletedItem = deletedItem.substring(1);
+
+  //   // delete the requested item from mongodb
+  //   Todo.find({ item: deletedItem }).remove( (err, data) => {
+  //     if (err) throw err;
+  //     res.json(data);
+  //   })
+  // });
+
+}
--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const express = require('express'),
 
 messengerWebbhookController = require('./controllers/messengerWebhook'),
 
+formController = require('./controllers/formController'),
+
 app = express();
 
 
+//set up template engine
+app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static('./public'));
+
 // Fire controllers
 messengerWebbhookController(app);
+formController(app);
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
