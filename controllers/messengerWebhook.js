3fd67f2a9e3b93c166804c9e3fd15d2c7a90545b
SL0TR <mohaiminislam94@gmail.com>
2018-08-14 16:12:36 +0600

    debug the things

--- controllers/messengerWebhook.js
+++ controllers/messengerWebhook.js
@@ -228,7 +228,7 @@ module.exports = (app) => {
 
       // The whole response has been received. Print out the result.
       resp.on('end', () => {
-        console.log(JSON.stringify(data).explanation);
+        console.log(data);
       });
 
     }).on("error", (err) => {
